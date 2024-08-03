f6e2b6a35a14d6ab5f1683beab85065d25014e50
juanpablow <juan.santana.macedo10@outlook.com>
2024-08-03 02:14:19 -0300

    feat: add `WorkItems` initialization and load_work_item method to `BotScraper`

--- BotScraper.py
+++ BotScraper.py
@@ -1,5 +1,7 @@
 import logging
 
+from RPA.Robocorp.WorkItems import WorkItems
+from selenium.webdriver.common.keys import Keys
 
 from CustomSelenium import CustomSelenium
 
@@ -11,6 +13,48 @@ logging.basicConfig(
 class BotScraper(CustomSelenium):
     def __init__(self):
         super().__init__()
+        self.work_items = WorkItems()
+        self.search_phrase = ""
+        self.category = ""
+        self.months = 0
+        self.load_work_item()
+
+    def load_work_item(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        work_items_path = os.path.join(current_dir, "devdata", "work-items.json")
+        with open(work_items_path, "r") as f:
+            work_item = json.load(f)
+
+        try:
+            self.search_phrase = work_item["search_phrase"]
+        except KeyError:
+            logging.error(
+                "The 'search_phrase' is mandatory and was not provided in the work item."
+            )
+            raise ValueError(
+                "The 'search_phrase' is mandatory and was not provided in the work item."
+            )
+
+        try:
+            self.category = work_item["category"]
+        except KeyError:
+            logging.warning("The 'category' was not provided in the work item.")
+            self.category = None
+
+        try:
+            self.months = int(work_item["months"])
+            if self.months < 0:
+                logging.warning(
+                    "The 'months' provided is less than 0. Defaulting to 0."
+                )
+                self.months = 0
+        except (KeyError, ValueError):
+            logging.warning(
+                "The 'months' was not provided or is not a valid integer in the work item, defaulting to 0."
+            )
+            self.months = 0
+        if self.months == 0:
+            self.months = 1
 
     def open_website(self, url):
         self.open_browser()
